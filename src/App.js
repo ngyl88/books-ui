1b1831e3b6fbe40768b766c45ca7140e4db44c1a
ngyl88 <ng_yl88@hotmail.com>
2018-07-25 16:50:15 +0800

    Added display of authors
    - remove dummy console log

--- src/App.js
+++ src/App.js
@@ -1,40 +1,50 @@
 import React, { Component } from "react";
 import "./App.css";
 
+const API_HOST = process.env.REACT_APP_API || "http://localhost:3000";
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      books: []
+      books: [],
+      authors: []
     };
   }
 
   async getBooks() {
-    console.log('....');
-    const apiHost = process.env.REACT_APP_API || "http://localhost:3000";
-    const response = await fetch(apiHost + "/books");
-    if (response.ok) {
-      return await response.json();
-    } else {
-      return [];
-    }
-  }
-  async componentDidMount() {
-    const data = await this.getBooks();
+    const response = await fetch(`${API_HOST}/books`);
+    const data = response.ok ? await response.json() : [];
     this.setState({
       books: data
     });
+  }
+  async getAuthors() {
+    const response = await fetch(`${API_HOST}/authors`);
+    const data = response.ok ? await response.json() : [];
+    this.setState({
+      authors: data
+    });
+  }
+  componentDidMount() {
+    this.getBooks();
+    this.getAuthors();
   }
 
   render() {
     return (
       <div>
+        <h3>Books</h3>
         {this.state.books.map(book => {
           return (
             <li key={book._id}>
               {book.title}, by {book.author ? book.author.name : ""}
             </li>
           );
+        })}
+        <h3>Authors</h3>
+        {this.state.authors.map(author => {
+          return <li key={author._id}>{author.name}</li>;
         })}
       </div>
     );
